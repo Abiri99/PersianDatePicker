dfc01cd7ee32e05dfe46e0db1ce3b77e6ecd7fa0
Ali-Lenovo <abiriali78@gmail.com>
2019-10-04 18:27:09 +0330

    pageview added to dialog

--- lib/home.dart
+++ lib/home.dart
@@ -1,28 +1,56 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  final controller = PageController(initialPage: 0);
+
+  dialogContent(BuildContext context) {
+    return Container(
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.red,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              height: 50,
+              child: PageView.builder(
+                itemBuilder: (context, index) {
+                  return Align(
+                    child: Text("${index}"),
+                  );
+                },
+                itemCount: 4,
+              ),
+            )
+          ],
+        ));
+  }
 
   opendatepicker(BuildContext context) {
-
     print("opendatepicker");
 
     var alertDialog = AlertDialog(
-      title: Text("date picker"),
-      content: Text("Choose a date"),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      title: null,
+      content: dialogContent(context),
     );
 
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alertDialog;
-      }
-    );
+        context: context,
+        builder: (BuildContext context) {
+          return alertDialog;
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: null,
+      appBar: AppBar(
+        title: Text("persian date picker"),
+      ),
       backgroundColor: Colors.blue,
       body: Center(
         child: RaisedButton(

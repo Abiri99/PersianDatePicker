8d8d9e1e22696160fb73ff820ea657bce5b0c9b5
Ali-Lenovo <abiriali78@gmail.com>
2019-10-05 20:53:45 +0330

    animation added

--- lib/components/pdp_dialog.dart
+++ lib/components/pdp_dialog.dart
@@ -10,44 +10,22 @@ class _pdpDialogState extends State<pdpDialog> {
   double _height = 400;
   var _fColor = Colors.white, _sColor = Color(0xFF203858);
   double _cHeight = 68, _cWidth = 68;
+  var _textColor = Colors.blueGrey;
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(8),
       ),
-      title: GestureDetector(
-        onTap: () {
-          print("hiding content");
-          // setState(() {
-          //   _visible = !_visible;
-          // });
-          // print(_visible);
-        },
-        child: Align(
-            alignment: Alignment.centerRight,
-            child: Card(
-              elevation: 8,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(100.0),
-              ),
-              child: Container(
-                height: _cHeight,
-                width: _cWidth,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(100.0),
-                  color: _fColor,
-                ),
-              ),
-            )),
-      ),
+      title: null,
       content: pdp(),
       backgroundColor: _sColor,
       contentPadding: EdgeInsets.only(
         left: 0,
         right: 0,
-        top: 8,
-        bottom: 8,
+        top: 0,
+        bottom: 0,
       ),
     );
   }
--- lib/components/persian_date_picker.dart
+++ lib/components/persian_date_picker.dart
@@ -1,88 +1,104 @@
 import 'package:flutter/material.dart';
 
 class pdp extends StatefulWidget {
+  var height = 350;
   @override
   _pdpState createState() => _pdpState();
 }
 
 class _pdpState extends State<pdp> {
   var _visible = true;
-  double _height = 350;
+  double _height1 = 50;
+  double _height2 = 300;
   @override
   Widget build(BuildContext context) {
     print("hi from pdp");
-    return AnimatedContainer(
-      duration: Duration(seconds: 1),
-      height: _height,
-      width: MediaQuery.of(context).size.width * 0.8,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      // visible: _visible,
-      child: Visibility(
-        visible: _visible,
+    return Container(
+        height: 350,
+        width: 500,
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             GestureDetector(
               onTap: () {
                 setState(() {
-                  _height = 0;
+                  _height1 = widget.height - _height1;
+                  _height2 = widget.height - _height2;
                 });
-                print(_visible);
               },
-              child: Text(
-                "سال",
-                style: Theme.of(context).textTheme.title,
-              ),
-            ),
-            Container(
-                height: 35,
+              child: AnimatedContainer(
+                duration: Duration(seconds: 1),
+                height: _height1,
                 width: double.infinity,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    Text("flash"),
-                    Container(
-                      width: 100,
-                      child: PageView.builder(
-                        physics: BouncingScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          return Align(
-                            child: Text(
-                              "${index}",
-                              style: Theme.of(context).textTheme.body1,
-                            ),
-                          );
-                        },
-                        itemCount: 4,
-                      ),
-                    ),
-                    Text("flash"),
-                  ],
-                )),
-            Text(
-              "ماه",
-              style: Theme.of(context).textTheme.body1,
-            ),
-            Container(
-              height: 25,
-              child: PageView.builder(
-                itemBuilder: (context, index) {
-                  return Align(
-                    child: Text(
-                      "${index}",
-                      style: Theme.of(context).textTheme.body1,
-                    ),
-                  );
-                },
-                itemCount: 4,
+                color: Colors.white,
+                child: Text("animated"),
               ),
             ),
+            Container(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _height2 = 0;
+                      });
+                      print(_visible);
+                    },
+                    child: Text(
+                      "سال",
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                  ),
+                  Container(
+                      height: 35,
+                      width: double.infinity,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Text("flash"),
+                          Container(
+                            width: 100,
+                            child: PageView.builder(
+                              physics: BouncingScrollPhysics(),
+                              itemBuilder: (context, index) {
+                                return Align(
+                                  child: Text(
+                                    "${index}",
+                                    style: Theme.of(context).textTheme.body1,
+                                  ),
+                                );
+                              },
+                              itemCount: 4,
+                            ),
+                          ),
+                          Text("flash"),
+                        ],
+                      )),
+                  Text(
+                    "ماه",
+                    style: Theme.of(context).textTheme.body1,
+                  ),
+                  Container(
+                    height: 25,
+                    child: PageView.builder(
+                      itemBuilder: (context, index) {
+                        return Align(
+                          child: Text(
+                            "${index}",
+                            style: Theme.of(context).textTheme.body1,
+                          ),
+                        );
+                      },
+                      itemCount: 4,
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
-      ),
     );
   }
 }
